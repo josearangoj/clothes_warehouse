7b1a83df97cddb93901ae524a9f046cdc047bbc1
Jose Arango <josearango1202@gmail.com>
2023-06-13 17:25:42 -0400

    imlementation of add item feature - include validation for some fields

---
+++ src/main/java/com/cpan228/clothes_warehouse/model/ItemModel.java
@@ -0,0 +1,63 @@
+package com.cpan228.clothes_warehouse.model;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
+
+public class ItemModel {
+
+    public enum FashionBrand {
+        BALENCIAGA("Balenciaga"),
+        STONE_ISLAND("Stone Island"),
+        DIOR("Dior"),
+        GUCCI("Gucci"),
+        VERSACE("Versace"),
+        LOUIS_VUITTON("Louis Vuitton");
+
+        private String brandName;
+
+        private FashionBrand(String brandName) {
+            this.brandName = brandName;
+        }
+
+        public String getBrandName() {
+            return brandName;
+        }
+
+    }
+
+    @Id
+    private Long id;
+
+    private FashionBrand brand;
+
+    @Min(value = 2021, message = "Year must be at least 2021")
+    private int yearOfCreation;
+
+    @Min(value = 1001, message = "Price must be higher than 1000")
+    private double price;
+
+    public FashionBrand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(FashionBrand brand) {
+        this.brand = brand;
+    }
+
+    public int getYearOfCreation() {
+        return yearOfCreation;
+    }
+
+    public void setYearOfCreation(int yearOfCreation) {
+        this.yearOfCreation = yearOfCreation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
